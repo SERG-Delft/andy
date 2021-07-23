e9d8391839999401a17fdbd55beadb9662a693fa
Jan Warchocki <janwarchocki@gmail.com>
2021-07-23 17:38:51 +0200

    Fixed the colon error for windows

--- src/test/java/nl/tudelft/cse1110/ResourceUtils.java
+++ src/test/java/nl/tudelft/cse1110/ResourceUtils.java
@@ -1,12 +1,14 @@
 package nl.tudelft.cse1110;
 
 import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ResourceUtils {
 
     public static String resourceFolder(String path) {
         try {
-            return ResourceUtils.class.getResource(path).toURI().getPath();
+            return Paths.get(ResourceUtils.class.getResource("/").toURI()).toString() + path;
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
