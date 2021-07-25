e4fa44f762f79672880afa4f8ce17413751edfa2
Jan Warchocki <janwarchocki@gmail.com>
2021-07-25 20:50:13 +0200

    Renamed jqwik test directories, added test to check if the message is shown if the error is not an assertion failure

--- src/test/java/nl/tudelft/cse1110/grader/integration/JQWikMessagesTest.java
+++ src/test/java/nl/tudelft/cse1110/grader/integration/JQWikMessagesTest.java
@@ -7,20 +7,19 @@ import static nl.tudelft.cse1110.grader.integration.GraderIntegrationTestHelper.
 import static nl.tudelft.cse1110.grader.integration.GraderIntegrationTestHelper.noScript;
 import static nl.tudelft.cse1110.grader.integration.GraderIntegrationTestAssertions.propertyTestFailing;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.setRemoveAssertJRelatedElementsFromStackTrace;
 
 public class JQWikMessagesTest extends GraderIntegrationTestBase {
 
     @Test
     void testSimplePropertyTest() {
-        String result = run(justTests(), noScript(), "jqwik/jqwikSimple");
+        String result = run(justTests(), noScript(), "jqwik/simple");
         assertThat(result)
                 .has(propertyTestFailing("testNoElementInWholeArray"));
     }
 
     @Test
     void testMultiplePropertyTestsFailing() {
-        String result = run(justTests(), noScript(), "jqwik/jqwikMultiple");
+        String result = run(justTests(), noScript(), "jqwik/multiple");
         assertThat(result)
                 .has(propertyTestFailing("testNoElementInWholeArray"))
                 .has(propertyTestFailing("testValueInArrayUniqueElements"));
@@ -28,11 +27,18 @@ public class JQWikMessagesTest extends GraderIntegrationTestBase {
 
     @Test
     void testMultiplePropertyWithParameterizedTests() {
-        String result = run(justTests(), noScript(), "jqwik/jqwikWithParameterized");
+        String result = run(justTests(), noScript(), "jqwik/withParameterized");
         assertThat(result)
                 .has(propertyTestFailing("testNoElementInWholeArray"))
                 .has(propertyTestFailing("testValueInArrayUniqueElements"))
                 .has(parameterizedTestFailing("test", 6));
+    }
+
+    @Test
+    void testMessageOtherThanAssertionError() {
+        String result = run(justTests(), noScript(), "jqwik/runtimeException");
+        assertThat(result)
+                .has(propertyTestFailing("testAddition"));
     }
 
 }
