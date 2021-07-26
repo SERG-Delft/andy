d10c81df382dd716851646e76978ebe9e88a55c3
Teo <T.Oprescu@student.tudelft.nl>
2021-07-26 23:27:01 +0300

    JARs get downloaded in resources folder, not target

--- src/test/java/nl/tudelft/cse1110/ResourceUtils.java
+++ src/test/java/nl/tudelft/cse1110/ResourceUtils.java
@@ -6,11 +6,30 @@ import java.nio.file.Paths;
 
 public class ResourceUtils {
 
+    public static final String LIBS_FOLDER_PATH = "\\grader\\src\\test\\resources\\grader\\libs\\";
+
     public static String resourceFolder(String path) {
         try {
             return Paths.get(ResourceUtils.class.getResource("/").toURI()).toString() + path;
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //JARs are downloaded in the resources folder, not in the temporary target folder
+    //Thus, they need to be downloaded only once
+    public static String permanentResourceFolder(){
+
+        Path path = null;
+        try {
+            path = Path.of(Paths.get(ResourceUtils.class.getResource("/").toURI()).toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        path = Paths.get(path.toString(), "../../../");
+        path = Paths.get(path.toString(), LIBS_FOLDER_PATH);
+
+        return path.toString();
     }
 }
