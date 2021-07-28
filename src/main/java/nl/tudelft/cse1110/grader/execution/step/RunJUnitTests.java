48f9a450f9b78477dbb64a04ab742900f11e6c01
Florena Buse <i.buse@student.tudelft.nl>
2021-07-28 11:50:57 +0200

    Small changes

--- src/main/java/nl/tudelft/cse1110/grader/execution/step/RunJUnitTests.java
+++ src/main/java/nl/tudelft/cse1110/grader/execution/step/RunJUnitTests.java
@@ -45,7 +45,7 @@ public class RunJUnitTests implements ExecutionStep {
 
             TestExecutionSummary summary = listener.getSummary();
 
-            if (!helper(cfg) || summary.getTestsFoundCount() == 0) {
+            if (!testsCheck(cfg) || summary.getTestsFoundCount() == 0) {
                 result.noTestsFound();
             } else {
                 result.logJUnitRun(summary);
@@ -56,22 +56,23 @@ public class RunJUnitTests implements ExecutionStep {
 
     }
 
+
     /**
-     * This method checks whether @Test can be found at least once in the student's solution.
+     * This method checks whether @.*Test can be found at least once in the student's solution.
      * If not, it means they have not written any tests.
      *
      * @param cfg the default configuration of the directories
      * @return false if no tests found, true if test(s) found
      * @throws IOException throws an IOException
      */
-    public boolean helper(Configuration cfg) throws IOException {
+    public boolean testsCheck(Configuration cfg) throws IOException {
         List<String> listOfFiles = filePathsAsString(getAllJavaFiles(cfg.getWorkingDir()));
         int count = 0;
 
         for(String pathOfJavaClass : listOfFiles) {
             String content = new String(Files.readAllBytes(Paths.get(pathOfJavaClass)));
 
-            String regex = "@.*Test+";
+            String regex = "@.*Test";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(content);
 
