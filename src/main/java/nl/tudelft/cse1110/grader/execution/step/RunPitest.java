b1c4d2fea9b90431443ab515ce50e3a6a5827244
Mauricio Aniche <mauricioaniche@gmail.com>
2021-08-16 12:47:03 +0200

    extracting the reports only after pitest actually generates them

--- src/main/java/nl/tudelft/cse1110/grader/execution/step/RunPitest.java
+++ src/main/java/nl/tudelft/cse1110/grader/execution/step/RunPitest.java
@@ -3,10 +3,11 @@ package nl.tudelft.cse1110.grader.execution.step;
 import nl.tudelft.cse1110.grader.config.Configuration;
 import nl.tudelft.cse1110.grader.config.DirectoryConfiguration;
 import nl.tudelft.cse1110.grader.config.RunConfiguration;
-import nl.tudelft.cse1110.grader.util.ClassUtils;
 import nl.tudelft.cse1110.grader.execution.ExecutionStep;
 import nl.tudelft.cse1110.grader.result.ResultBuilder;
+import nl.tudelft.cse1110.grader.util.ClassUtils;
 import nl.tudelft.cse1110.grader.util.FileUtils;
+import org.jetbrains.annotations.NotNull;
 import org.pitest.mutationtest.commandline.OptionsParser;
 import org.pitest.mutationtest.commandline.ParseResult;
 import org.pitest.mutationtest.commandline.PluginFilter;
@@ -18,12 +19,12 @@ import org.pitest.mutationtest.tooling.EntryPoint;
 import org.pitest.util.Unchecked;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static nl.tudelft.cse1110.grader.util.FileUtils.concatenateDirectories;
 
 public class RunPitest implements ExecutionStep {
 
@@ -32,7 +33,9 @@ public class RunPitest implements ExecutionStep {
     public void execute(Configuration cfg, ResultBuilder result) {
         final PluginServices plugins = PluginServices.makeForContextLoader();
         final OptionsParser parser = new OptionsParser(new PluginFilter(plugins));
-        final ParseResult pr = parser.parse(buildArgs(cfg));
+
+        String outputPitestDir = createDirectoryForPitest(cfg);
+        final ParseResult pr = parser.parse(buildArgs(cfg, outputPitestDir));
 
         if (!pr.isOk()) {
             result.genericFailure(this, pr.getErrorMessage().get());
@@ -40,24 +43,31 @@ public class RunPitest implements ExecutionStep {
             final ReportOptions data = pr.getOptions();
             final CombinedStatistics stats = runReport(data, plugins);
 
+            // We "skip" this folder and instead keep its contents
+
+            extractAndRemoveReportFolder(outputPitestDir);
+
             result.logPitest(stats);
         }
     }
 
-    private String[] buildArgs(Configuration cfg) {
+    @NotNull
+    private String createDirectoryForPitest(Configuration cfg) {
+        // Create new directory ".../output/pitest"
+        // Here, the PiTest report folder in format "year-month-day-time" will be written
+        String outputPitestDir = concatenateDirectories(cfg.getDirectoryConfiguration().getOutputDir(), "pitest");
+        FileUtils.createDirIfNeeded(outputPitestDir);
+        return outputPitestDir;
+    }
+
+    private String[] buildArgs(Configuration cfg, String pitestOutputDir) {
         DirectoryConfiguration dirCfg = cfg.getDirectoryConfiguration();
         RunConfiguration runCfg = cfg.getRunConfiguration();
 
         List<String> args = new ArrayList<>();
 
         args.add("--reportDir");
-        // Create new directory ".../output/pitest"
-        // Here, the PiTest report folder in format "year-month-day-time" will be written
-        String outputPitestDir = FileUtils.concatenateDirectories(dirCfg.getOutputDir(), "pitest");
-        FileUtils.createDirIfNeeded(outputPitestDir);
-        args.add(outputPitestDir);
-        // We "skip" this folder and instead keep its contents
-        extractAndRemoveReportFolder(outputPitestDir);
+        args.add(pitestOutputDir);
 
         args.add("--targetClasses");
         args.add(commaSeparated(runCfg.classesUnderTest()));
@@ -119,7 +129,7 @@ public class RunPitest implements ExecutionStep {
                 }
                 FileUtils.deleteDirectory(reportFolderToSkip);
             } else {
-//                throw new RuntimeException("PiTest report was not written to .../output/pitest!");
+                throw new RuntimeException("PiTest report was not written to .../output/pitest!");
             }
 
         } catch (Exception e) {
