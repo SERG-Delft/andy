df157ef0619885b94666fa97572572d8c2f32825
Mauricio Aniche <mauricioaniche@gmail.com>
2021-08-17 09:34:39 +0200

    extracting some methods in the Runner; removing useless comments

--- src/main/java/nl/tudelft/cse1110/Runner.java
+++ src/main/java/nl/tudelft/cse1110/Runner.java
@@ -4,33 +4,41 @@ import nl.tudelft.cse1110.grader.config.Configuration;
 import nl.tudelft.cse1110.grader.config.DirectoryConfiguration;
 import nl.tudelft.cse1110.grader.execution.ExecutionFlow;
 import nl.tudelft.cse1110.grader.result.ResultBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class Runner {
 
     public static void main(String[] args) {
-        Configuration cfg = new Configuration();
+        Configuration cfg = buildConfiguration();
 
-//        DirectoryConfiguration dirCfg = new DirectoryConfiguration(
-//                "E:\\TUDelft\\CSE1110 Summer\\code",
-//                "E:\\TUDelft\\CSE1110 Summer\\output"
-//        );
+        ResultBuilder result = new ResultBuilder();
+        ExecutionFlow flow = buildExecutionFlow(cfg, result);
+
+        if(flow == null) {
+            System.out.println("Unknown mode");
+            System.exit(-1);
+        }
+
+        flow.run();
+        System.out.println(result.buildDebugResult());
+    }
+
+    @NotNull
+    private static Configuration buildConfiguration() {
+        Configuration cfg = new Configuration();
 
         DirectoryConfiguration dirCfg = new DirectoryConfiguration(
                 System.getenv("WORKING_DIR"),
                 System.getenv("OUTPUT_DIR")
         );
-
         cfg.setDirectoryConfiguration(dirCfg);
+        return cfg;
+    }
 
-        ResultBuilder result = new ResultBuilder();
-
-//        ExecutionFlow flow = ExecutionFlow.fullMode(cfg, result);
-//        flow.run();
-//
-//        System.out.println(result.buildDebugResult());
-
+    @Nullable
+    private static ExecutionFlow buildExecutionFlow(Configuration cfg, ResultBuilder result) {
         String mode = System.getenv("MODE");
-
         ExecutionFlow flow = null;
         if (mode.equals("FULL")) {
             flow = ExecutionFlow.fullMode(cfg, result);
@@ -39,13 +47,6 @@ public class Runner {
         } else if (mode.equals("TESTS")) {
             flow = ExecutionFlow.justTests(cfg, result);
         }
-
-        if (flow != null) {
-            flow.run();
-
-            System.out.println(result.buildDebugResult());
-        } else {
-            System.out.println("Unknown mode");
-        }
+        return flow;
     }
 }
