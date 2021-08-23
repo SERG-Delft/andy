62cd2e8c21c74c76070b9e1be4b63acf78179864
Jan Warchocki <janwarchocki@gmail.com>
2021-08-23 15:18:20 +0200

    Fix compilation error

--- src/main/java/nl/tudelft/cse1110/andy/grader/execution/step/GetRunConfigurationStep.java
+++ src/main/java/nl/tudelft/cse1110/andy/grader/execution/step/GetRunConfigurationStep.java
@@ -15,7 +15,6 @@ import java.util.NoSuchElementException;
 import static nl.tudelft.cse1110.andy.grader.execution.step.helper.ModeSelector.HINTS;
 import static nl.tudelft.cse1110.andy.grader.util.ClassUtils.allClassesButTestingAndConfigOnes;
 import static nl.tudelft.cse1110.andy.grader.util.ClassUtils.getConfigurationClass;
-import static nl.tudelft.cse1110.andy.grader.util.ModeUtils.*;
 import static nl.tudelft.cse1110.andy.grader.config.RunConfiguration.*;
 
 public class GetRunConfigurationStep implements ExecutionStep {
