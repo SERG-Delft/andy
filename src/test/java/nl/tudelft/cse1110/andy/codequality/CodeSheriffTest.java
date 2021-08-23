6cce26e28067d3952ccde2e3285db40ac71dd3e1
Mauricio Aniche <mauricioaniche@gmail.com>
2021-08-23 09:54:55 +0200

    configuring codesheriff in our code base

---
+++ src/test/java/nl/tudelft/cse1110/andy/codequality/CodeSheriffTest.java
@@ -0,0 +1,33 @@
+package nl.tudelft.cse1110.andy.codequality;
+
+import com.github.mauricioaniche.codesheriff.dsl.CodeSheriff;
+import com.github.mauricioaniche.codesheriff.junit.CodeSheriffJUnit5;
+
+public class CodeSheriffTest extends CodeSheriffJUnit5 {
+
+    CodeSheriff complexity() {
+        CodeSheriff sheriff = new CodeSheriff();
+
+        sheriff.thatEnsures()
+                .methods()
+                .inClassesOfPackage("nl.tudelft.cse1110.andy")
+                .have()
+                .complexity(m -> m <= 10);
+
+        return sheriff;
+    }
+
+    CodeSheriff loc() {
+        CodeSheriff sheriff = new CodeSheriff();
+
+        sheriff.thatEnsures()
+                .methods()
+                .inClassesOfPackage("nl.tudelft.cse1110.andy")
+                .withExceptionOfMethod("execute/2") // the Step methods are a bit long now, we gotta refactor them
+                .have()
+                .linesOfCode(m -> m <= 25);
+
+        return sheriff;
+    }
+
+}
