b7fa70bbc3ecb9f289607cb27abc49fd37bca4a0
Jan Warchocki <janwarchocki@gmail.com>
2021-08-26 21:37:34 +0200

    Added an explanatory comment regarding Action.CUSTOM

--- src/main/java/nl/tudelft/cse1110/andy/grader/execution/step/helper/Action.java
+++ src/main/java/nl/tudelft/cse1110/andy/grader/execution/step/helper/Action.java
@@ -1,5 +1,7 @@
 package nl.tudelft.cse1110.andy.grader.execution.step.helper;
 
+// CUSTOM mode is to be used during tests and meta tests.
+// This action means that the steps for the execution flow have already been set.
 public enum Action {
     FULL_WITH_HINTS, FULL_WITHOUT_HINTS, COVERAGE, TESTS, CUSTOM
 }
