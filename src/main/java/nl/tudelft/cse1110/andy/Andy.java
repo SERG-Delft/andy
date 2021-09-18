aad57216f81c16e3b0af2e34e3948834f971f532
Mauricio Aniche <mauricioaniche@gmail.com>
2021-09-18 14:46:28 +0200

    simplifying the CLI for the maven mojo

--- src/main/java/nl/tudelft/cse1110/andy/Andy.java
+++ src/main/java/nl/tudelft/cse1110/andy/Andy.java
@@ -1,101 +1,59 @@
 package nl.tudelft.cse1110.andy;
 
-import com.google.common.io.Files;
 import nl.tudelft.cse1110.andy.config.DirectoryConfiguration;
 import nl.tudelft.cse1110.andy.execution.Context;
 import nl.tudelft.cse1110.andy.execution.ExecutionFlow;
 import nl.tudelft.cse1110.andy.execution.mode.Action;
 import nl.tudelft.cse1110.andy.grade.GradeCalculator;
-import nl.tudelft.cse1110.andy.writer.weblab.RandomAsciiArtGenerator;
 import nl.tudelft.cse1110.andy.result.ResultBuilder;
 import nl.tudelft.cse1110.andy.writer.ResultWriter;
+import nl.tudelft.cse1110.andy.writer.weblab.RandomAsciiArtGenerator;
 import nl.tudelft.cse1110.andy.writer.weblab.WebLabResultWriter;
-import org.apache.commons.cli.*;
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-
-import static nl.tudelft.cse1110.andy.utils.FilesUtils.*;
 
 public class Andy {
 
-    private static final CommandLineParser parser = new DefaultParser();
-    private static final Options options          = new Options();
-    private static File temporaryDirectory        = null;
+    private final String action;
+    private final String workDir;
+    private final String outputDir;
 
-    public static void main(String[] args) throws Exception {
-        Context ctx = (args.length > 0)
-                ? buildContextWithCommandLineArguments(args)
-                : buildContextWithEnvironmentVariables();
+    public static void main(String[] args) {
+        String action = args[0];
+        String workDir = args[1];
+        String outputDir = args[2];
+
+        if (action == null) { System.out.println("No ACTION environment variable.");      System.exit(-1); }
+        if (workDir == null) { System.out.println("No WORKING_DIR environment variable."); System.exit(-1); }
+        if (outputDir == null) { System.out.println("No OUTPUT_DIR environment variable.");  System.exit(-1); }
+
+        new Andy(action, workDir, outputDir).assess();
+    }
+
+    public Andy(String action, String workDir, String outputDir) {
+        assert action!=null;
+        assert workDir!=null;
+        assert outputDir!=null;
+
+        this.action = action;
+        this.workDir = workDir;
+        this.outputDir = outputDir;
+    }
+
+    public void assess() {
+        Context ctx = buildContext();
 
         ResultBuilder result = new ResultBuilder(ctx, new GradeCalculator());
         ResultWriter writer = new WebLabResultWriter(ctx, new RandomAsciiArtGenerator());
         ExecutionFlow flow = ExecutionFlow.build(ctx, result, writer);
 
         flow.run();
-
-        System.out.println(readFile(new File(concatenateDirectories(ctx.getDirectoryConfiguration().getOutputDir(), "stdout.txt"))));
-
-        /* Remove the temporary directory, if one was created. */
-        if (temporaryDirectory != null) {
-            FileUtils.deleteDirectory(temporaryDirectory);
-        }
     }
 
-    private static Context buildContextWithCommandLineArguments(String[] args) throws Exception {
-        options.addOption("e", "exercise", true, "Exercise name in the assignments repo.");
-        options.addOption("d", "directory", true, "Base directory of the exercise");
-        options.addOption("o", "output", true, "Output directory of the exercise");
-        options.addOption("f", "full-with-hints", false, "Run Andy completely");
-        options.addOption("F", "full-without-hints", false, "Run Andy without hints");
-        options.addOption("c", "coverage", false, "Run Andy with only coverage");
-        options.addOption("t", "tests", false, "Run Andy only with tests");
-        options.addOption("T", "meta-tests", false, "Run Andy only with meta tests");
+    private Context buildContext() {
+        Context ctx = new Context(Action.valueOf(action));
 
-        CommandLine cmd = parser.parse(options, args);
-        if (cmd.hasOption('e')) return buildContext(cmd);
-        if (!cmd.hasOption('d')) { System.out.println("Error: no base directory supplied, please use the flag -d.");   System.exit(-1); }
-        if (!cmd.hasOption('o')) { System.out.println("Error: no output directory supplied, please use the flag -o."); System.exit(-1); }
-
-        return buildContext(cmd);
-    }
-
-    private static Context buildContextWithEnvironmentVariables() throws Exception {
-        if (System.getenv("ACTION")      == null) { System.out.println("No ACTION environment variable.");      System.exit(-1); }
-        if (System.getenv("WORKING_DIR") == null) { System.out.println("No WORKING_DIR environment variable."); System.exit(-1); }
-        if (System.getenv("OUTPUT_DIR")  == null) { System.out.println("No OUTPUT_DIR environment variable.");  System.exit(-1); }
-
-        return buildContext(null);
-    }
-
-    private static Context buildContext(CommandLine cmd) throws FileNotFoundException {
-        Action action = (cmd == null) ? Action.valueOf(System.getenv("ACTION"))
-                : cmd.hasOption('f') ? Action.FULL_WITH_HINTS
-                : cmd.hasOption('F') ? Action.FULL_WITHOUT_HINTS
-                : cmd.hasOption('c') ? Action.COVERAGE
-                : cmd.hasOption('T') ? Action.META_TEST
-                : Action.TESTS;
-        Context ctx = new Context(action);
-
-        DirectoryConfiguration dirCfg;
-        if (cmd == null) {
-            dirCfg = new DirectoryConfiguration(System.getenv("WORKING_DIR"), System.getenv("OUTPUT_DIR"));
-        } else {
-            temporaryDirectory  = Files.createTempDir();
-
-            /* Move all the files over to the temporary directory. */
-            String workDir = (cmd.hasOption('e'))
-                    ? System.getProperty("user.dir") + "/output/" + cmd.getOptionValue('e')
-                    : cmd.getOptionValue('d');
-            copyFile(findLibrary(workDir), temporaryDirectory.getAbsolutePath());
-            copyFile(findSolution(workDir), temporaryDirectory.getAbsolutePath());
-            copyFile(findConfiguration(workDir), temporaryDirectory.getAbsolutePath());
-
-            dirCfg = (cmd.hasOption('e'))
-                   ? new DirectoryConfiguration(temporaryDirectory.getAbsolutePath(), System.getProperty("user.dir") + "/output/" + cmd.getOptionValue('e'))
-                   : new DirectoryConfiguration(temporaryDirectory.getAbsolutePath(), cmd.getOptionValue('o'));
-        }
+        DirectoryConfiguration dirCfg = new DirectoryConfiguration(
+                workDir,
+                outputDir);
 
         ctx.setDirectoryConfiguration(dirCfg);
 
