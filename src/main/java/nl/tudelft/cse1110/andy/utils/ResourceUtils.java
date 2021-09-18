5b3f3051751832cd460d942229c11cc2f5736fd4
Mauricio Aniche <mauricioaniche@gmail.com>
2021-09-18 22:43:03 +0200

    move read congrats file logic to the random ascii art class

--- src/main/java/nl/tudelft/cse1110/andy/utils/ResourceUtils.java
+++ src/main/java/nl/tudelft/cse1110/andy/utils/ResourceUtils.java
@@ -1,12 +1,5 @@
 package nl.tudelft.cse1110.andy.utils;
 
-import org.apache.commons.io.IOUtils;
-
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
-
 public class ResourceUtils {
 
     public static String resourceFolder(String path) {
@@ -17,30 +10,4 @@ public class ResourceUtils {
         }
     }
 
-    public static List<String> listOfCongratsFiles() {
-        try {
-            InputStream resourceAsStream = ResourceUtils.class.getResourceAsStream("/congrats/0list.txt");
-            String list = IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
-
-            List<String> allFiles = Arrays.asList(list.split("\n"));
-
-            if(allFiles.isEmpty())
-                throw new RuntimeException("No congrats files!");
-
-            return allFiles;
-
-        } catch(Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String readCongratsFile(String randomAsciiFile) {
-        try {
-            InputStream resourceAsStream = ResourceUtils.class.getResourceAsStream("/congrats/" + randomAsciiFile);
-            return IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
-        } catch(Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 }
--- src/main/java/nl/tudelft/cse1110/andy/writer/weblab/RandomAsciiArtGenerator.java
+++ src/main/java/nl/tudelft/cse1110/andy/writer/weblab/RandomAsciiArtGenerator.java
@@ -1,10 +1,13 @@
 package nl.tudelft.cse1110.andy.writer.weblab;
 
+import nl.tudelft.cse1110.andy.utils.ResourceUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
-import static nl.tudelft.cse1110.andy.utils.ResourceUtils.listOfCongratsFiles;
-import static nl.tudelft.cse1110.andy.utils.ResourceUtils.readCongratsFile;
 
 public class RandomAsciiArtGenerator {
 
@@ -19,6 +22,34 @@ public class RandomAsciiArtGenerator {
         String randomAsciiFile = congratsFiles.get(random.nextInt(congratsFiles.size()));
 
         return readCongratsFile(randomAsciiFile);
+    }
+
+
+    private List<String> listOfCongratsFiles() {
+        try {
+            InputStream resourceAsStream = ResourceUtils.class.getResourceAsStream("/congrats/0list.txt");
+            String list = IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
+
+            List<String> allFiles = Arrays.asList(list.split("\n"));
+
+            if(allFiles.isEmpty())
+                throw new RuntimeException("No congrats files!");
+
+            return allFiles;
+
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String readCongratsFile(String randomAsciiFile) {
+        try {
+            InputStream resourceAsStream = ResourceUtils.class.getResourceAsStream("/congrats/" + randomAsciiFile);
+            return IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
