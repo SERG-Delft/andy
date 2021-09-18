efa2914e740e53acbc6e1c71eaedb93c50570bfb
Mauricio Aniche <mauricioaniche@gmail.com>
2021-09-18 15:31:50 +0200

    first working version of the mojo

--- src/main/java/com/github/cse1110/andy/AndyMojo.java
+++ src/main/java/com/github/cse1110/andy/AndyMojo.java
@@ -1,21 +1,74 @@
 package com.github.cse1110.andy;
 
+import com.google.common.io.Files;
+import nl.tudelft.cse1110.andy.utils.FilesUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import nl.tudelft.cse1110.andy.Andy;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static nl.tudelft.cse1110.andy.utils.FilesUtils.*;
 
 @Mojo(name = "andy")
 public class AndyMojo extends AbstractMojo {
 
-    @Parameter(property = "exercise", required = true)
-    private String exercise;
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    protected MavenProject project;
+
+    @Parameter(property = "action", defaultValue = "FULL_WITHOUT_HINTS")
+    private String action;
 
     @Override
     public void execute() {
+        File basedir = project.getBasedir();
+
+        File workDir = null;
         try {
-            Andy.main(new String[] { "-e", exercise });
-        } catch (Exception ignored) {}
+
+            /**
+             * We should first create a temporary work directory and
+             * copy the student solution, the config file, and the library code
+             * to it.
+             */
+            workDir = Files.createTempDir();
+
+            Collection<File> javaFiles = getAllJavaFiles(basedir.getAbsolutePath());
+            for (File javaFile : javaFiles) {
+                copyFile(javaFile.getAbsolutePath(), workDir.getAbsolutePath());
+            }
+
+            /**
+             * Create an output directory where we generate all the reports.
+             * We delete it first, because there might be one already, from a
+             * previous execution.
+             */
+            File outputDir = new File(concatenateDirectories(basedir.getAbsolutePath(), "output"));
+            if(outputDir.exists()) {
+                deleteDirectory(outputDir);
+            }
+            createDirIfNeeded(outputDir.getAbsolutePath());
+
+            /* Run Andy! */
+            new Andy(action, workDir.getAbsolutePath(), outputDir.getAbsolutePath()).assess();
+
+            /* Read output file */
+            String output = readFile(new File(concatenateDirectories(outputDir.getAbsolutePath(), "stdout.txt")));
+            System.out.println(output);
+
+        } catch (Exception ex) {
+            ex.printStackTrace(System.err);
+            System.exit(-1);
+        } finally {
+            // Delete the work dir as it's not needed anymore
+            if(workDir!=null)
+                deleteDirectory(workDir);
+        }
     }
 
 }
