753a0cc6c0a861f88e148135bd8091db9faf500b
Mauricio Aniche <mauricioaniche@gmail.com>
2021-09-22 09:56:29 +0200

    handling bad arguments when calling andy on weblab

--- src/main/java/nl/tudelft/cse1110/andy/AndyOnWebLab.java
+++ src/main/java/nl/tudelft/cse1110/andy/AndyOnWebLab.java
@@ -4,9 +4,17 @@ import nl.tudelft.cse1110.andy.execution.mode.Action;
 import nl.tudelft.cse1110.andy.writer.standard.RandomAsciiArtGenerator;
 import nl.tudelft.cse1110.andy.writer.weblab.WebLabResultWriter;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class AndyOnWebLab {
 
     public static void main(String[] args) {
+        if(args.length!=3) {
+            System.out.println("Andy needs three parameters: <ACTION/TASK_MODE> <WORK DIRECTORY> <OUTPUT DIRECTORY>");
+            System.exit(-1);
+        }
+
         String action = args[0];
         String workDir = args[1];
         String outputDir = args[2];
@@ -16,7 +24,18 @@ public class AndyOnWebLab {
         if (outputDir == null) { System.out.println("No OUTPUT_DIR environment variable.");  System.exit(-1); }
 
         WebLabResultWriter writer = new WebLabResultWriter(new RandomAsciiArtGenerator());
-        new Andy(Action.valueOf(action), workDir, outputDir, writer).run();
+        new Andy(getAction(action), workDir, outputDir, writer).run();
+    }
+
+    private static Action getAction(String action) {
+        try {
+            return Action.valueOf(action);
+        } catch(IllegalArgumentException e) {
+            System.out.println("Invalid action/task mode: " + action);
+            System.out.println("Valid ones: " + Arrays.stream(Action.values()).map(a -> a.toString()).collect(Collectors.joining(",")));
+            System.exit(-1);
+            return null;
+        }
     }
 
 }
