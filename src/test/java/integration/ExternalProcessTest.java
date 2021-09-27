bcbe8cbb4ab162869c018c53e51e4b88dda9e94c
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-09-28 00:48:26 +0200

    Add tests for external process

---
+++ src/test/java/integration/ExternalProcessTest.java
@@ -0,0 +1,33 @@
+package integration;
+
+import nl.tudelft.cse1110.andy.result.Result;
+import org.junit.jupiter.api.Test;
+
+import static java.time.Duration.ofSeconds;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+
+public class ExternalProcessTest extends IntegrationTestBase {
+
+    @Test
+    void externalProcessGracefulExit() {
+        Result result = run("EmptyLibrary", "EmptySolution",
+                "ExternalProcessGracefulExitConfiguration");
+
+        assertThat(result.getExternalProcessOutput()).isEqualTo("hello");
+    }
+
+    @Test
+    void externalProcessEndSignal() {
+        assertTimeout(ofSeconds(20), () -> {
+
+            Result result = run("EmptyLibrary", "EmptySolution",
+                    "ExternalProcessEndSignalConfiguration");
+
+            assertThat(result.getExternalProcessOutput())
+                    .startsWith("hello hello endsignal");
+
+        });
+    }
+}
---
+++ src/test/resources/grader/fixtures/Config/ExternalProcessEndSignalConfiguration.java
@@ -0,0 +1,34 @@
+package delft;
+
+import nl.tudelft.cse1110.andy.config.RunConfiguration;
+import nl.tudelft.cse1110.andy.config.MetaTest;
+import nl.tudelft.cse1110.andy.execution.process.ExternalProcess;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Configuration extends RunConfiguration {
+
+    @Override
+    public List<String> classesUnderTest() {
+        return List.of("delft.EmptyLibrary");
+    }
+
+    @Override
+    public Map<String, Float> weights() {
+        return new HashMap<>() {{
+            put("coverage", 0.0f);
+            put("mutation", 0.0f);
+            put("meta", 1.0f);
+            put("codechecks", 0.0f);
+        }};
+    }
+
+    @Override
+    public ExternalProcess externalProcess() {
+        return new ExternalProcess(
+                "sh -c \"echo hello hello endsignal hello hello hello && sleep 10000\"",
+                "endsignal");
+    }
+}
