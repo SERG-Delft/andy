6f3b4d9c9d989ae1c50ae000f9b0b2854a5d2bb4
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-09-28 22:09:26 +0200

    Run external process tests that rely on shell scripts only on Linux and Mac

--- src/test/java/integration/ExternalProcessTest.java
+++ src/test/java/integration/ExternalProcessTest.java
@@ -2,12 +2,14 @@ package integration;
 
 import nl.tudelft.cse1110.andy.result.Result;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static java.time.Duration.ofSeconds;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
-
+@EnabledOnOs({OS.LINUX, OS.MAC})
 public class ExternalProcessTest extends IntegrationTestBase {
 
     @Test
