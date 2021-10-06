f3742a560bd59407897d1e4bb15a351409a4529f
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-10-06 14:43:23 +0200

    Fix UseReflection security test triggering multiple security checks

--- src/test/resources/grader/fixtures/Solution/securitytests/UseReflection.java
+++ src/test/resources/grader/fixtures/Solution/securitytests/UseReflection.java
@@ -1,13 +1,12 @@
 package delft;
 
-import nl.tudelft.cse1110.andy.config.RunConfiguration;
 import org.junit.jupiter.api.Test;
 
 class ExploitTest {
 
     @Test
     void exploitTest() throws Exception {
-        var runConfigurationClass = Class.forName("delft.ExploitTest");
+        var test = Class.forName("delft.ExploitTest");
     }
 
 }
