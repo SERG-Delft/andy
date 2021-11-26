c7fd45aec0a01ff01642e69947e771e77c74d9b4
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-11-26 15:00:54 +0100

    Remove unnecessary imports and annotation

--- src/test/java/integration/SeleniumTest.java
+++ src/test/java/integration/SeleniumTest.java
@@ -2,21 +2,12 @@ package integration;
 
 import nl.tudelft.cse1110.andy.execution.mode.Action;
 import nl.tudelft.cse1110.andy.result.Result;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static java.time.Duration.ofSeconds;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
-@EnabledOnOs({OS.LINUX, OS.MAC})
 public class SeleniumTest extends IntegrationTestBase {
 
     @Test
