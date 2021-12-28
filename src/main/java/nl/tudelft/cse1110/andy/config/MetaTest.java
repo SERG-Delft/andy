ce35ddddc23b53e90f012e5440df02754d5ff584
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-28 23:37:50 +0200

    Add ExternalProcessMetaTest factory methods to facade

--- src/main/java/nl/tudelft/cse1110/andy/config/MetaTest.java
+++ src/main/java/nl/tudelft/cse1110/andy/config/MetaTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.cse1110.andy.config;
 
+import nl.tudelft.cse1110.andy.execution.externalprocess.ExternalProcess;
 import nl.tudelft.cse1110.andy.execution.metatest.AbstractMetaTestFactory;
 
 public interface MetaTest {
@@ -31,5 +32,13 @@ public interface MetaTest {
 
     static MetaTest insertAt(String name, int lineToInsertStartingIn1, String contentToAdd) {
         return new AbstractMetaTestFactory().insertAt(name, lineToInsertStartingIn1, contentToAdd);
+    }
+
+    static MetaTest withExternalProcess(int weight, String name, ExternalProcess externalProcess) {
+        return new AbstractMetaTestFactory().withExternalProcess(weight, name, externalProcess);
+    }
+
+    static MetaTest withExternalProcess(String name, ExternalProcess externalProcess) {
+        return new AbstractMetaTestFactory().withExternalProcess(name, externalProcess);
     }
 }
