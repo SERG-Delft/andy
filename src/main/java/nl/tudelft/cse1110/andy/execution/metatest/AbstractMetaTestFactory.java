9f3c5db089088b7cabef7dfd00c19d0e660c9dce
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-28 23:10:35 +0200

    Create AbstractMetaTestFactory

---
+++ src/main/java/nl/tudelft/cse1110/andy/execution/metatest/AbstractMetaTestFactory.java
@@ -0,0 +1,37 @@
+package nl.tudelft.cse1110.andy.execution.metatest;
+
+import nl.tudelft.cse1110.andy.execution.metatest.evaluators.InsertAtEvaluator;
+import nl.tudelft.cse1110.andy.execution.metatest.evaluators.LineReplacementEvaluator;
+import nl.tudelft.cse1110.andy.execution.metatest.evaluators.MetaEvaluator;
+import nl.tudelft.cse1110.andy.execution.metatest.evaluators.StringReplacementEvaluator;
+import nl.tudelft.cse1110.andy.execution.metatest.implementations.LibraryMetaTest;
+
+public class AbstractMetaTestFactory {
+
+    public AbstractMetaTest withStringReplacement(int weight, String name, String old, String replacement) {
+        MetaEvaluator evaluator = new StringReplacementEvaluator(old, replacement);
+        return new LibraryMetaTest(weight, name, evaluator);
+    }
+
+    public AbstractMetaTest withStringReplacement(String name, String old, String replacement) {
+        return withStringReplacement(1, name, old, replacement);
+    }
+
+    public AbstractMetaTest withLineReplacement(int weight, String name, int start, int end, String replacement) {
+        MetaEvaluator evaluator = new LineReplacementEvaluator(start, end, replacement);
+        return new LibraryMetaTest(weight, name, evaluator);
+    }
+
+    public AbstractMetaTest withLineReplacement(String name, int start, int end, String replacement) {
+        return withLineReplacement(1, name, start, end, replacement);
+    }
+
+    public AbstractMetaTest insertAt(int weight, String name, int lineToInsertStartingIn1, String contentToAdd) {
+        MetaEvaluator evaluator = new InsertAtEvaluator(lineToInsertStartingIn1, contentToAdd);
+        return new LibraryMetaTest(weight, name, evaluator);
+    }
+
+    public AbstractMetaTest insertAt(String name, int lineToInsertStartingIn1, String contentToAdd) {
+        return insertAt(1, name, lineToInsertStartingIn1, contentToAdd);
+    }
+}
