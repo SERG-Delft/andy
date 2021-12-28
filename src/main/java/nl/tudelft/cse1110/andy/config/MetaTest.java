d6dca1be7f8954468520b2c66834de1c4c226c1b
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-28 23:11:53 +0200

    Move factory methods to AbstractMetaTestFactory

--- src/main/java/nl/tudelft/cse1110/andy/config/MetaTest.java
+++ src/main/java/nl/tudelft/cse1110/andy/config/MetaTest.java
@@ -1,6 +1,6 @@
 package nl.tudelft.cse1110.andy.config;
 
-import nl.tudelft.cse1110.andy.execution.metatest.AbstractMetaTest;
+import nl.tudelft.cse1110.andy.execution.metatest.AbstractMetaTestFactory;
 
 public interface MetaTest {
     String getName();
@@ -10,26 +10,26 @@ public interface MetaTest {
     String getNameAndWeight();
 
     static MetaTest withStringReplacement(int weight, String name, String old, String replacement) {
-        return AbstractMetaTest.withStringReplacement(weight, name, old, replacement);
+        return new AbstractMetaTestFactory().withStringReplacement(weight, name, old, replacement);
     }
 
     static MetaTest withStringReplacement(String name, String old, String replacement) {
-        return AbstractMetaTest.withStringReplacement(name, old, replacement);
+        return new AbstractMetaTestFactory().withStringReplacement(name, old, replacement);
     }
 
     static MetaTest withLineReplacement(int weight, String name, int start, int end, String replacement) {
-        return AbstractMetaTest.withLineReplacement(weight, name, start, end, replacement);
+        return new AbstractMetaTestFactory().withLineReplacement(weight, name, start, end, replacement);
     }
 
     static MetaTest withLineReplacement(String name, int start, int end, String replacement) {
-        return AbstractMetaTest.withLineReplacement(name, start, end, replacement);
+        return new AbstractMetaTestFactory().withLineReplacement(name, start, end, replacement);
     }
 
     static MetaTest insertAt(int weight, String name, int lineToInsertStartingIn1, String contentToAdd) {
-        return AbstractMetaTest.insertAt(weight, name, lineToInsertStartingIn1, contentToAdd);
+        return new AbstractMetaTestFactory().insertAt(weight, name, lineToInsertStartingIn1, contentToAdd);
     }
 
     static MetaTest insertAt(String name, int lineToInsertStartingIn1, String contentToAdd) {
-        return AbstractMetaTest.insertAt(name, lineToInsertStartingIn1, contentToAdd);
+        return new AbstractMetaTestFactory().insertAt(name, lineToInsertStartingIn1, contentToAdd);
     }
 }
--- src/main/java/nl/tudelft/cse1110/andy/execution/metatest/AbstractMetaTest.java
+++ src/main/java/nl/tudelft/cse1110/andy/execution/metatest/AbstractMetaTest.java
@@ -1,11 +1,6 @@
 package nl.tudelft.cse1110.andy.execution.metatest;
 
 import nl.tudelft.cse1110.andy.config.MetaTest;
-import nl.tudelft.cse1110.andy.execution.metatest.evaluators.InsertAtEvaluator;
-import nl.tudelft.cse1110.andy.execution.metatest.evaluators.LineReplacementEvaluator;
-import nl.tudelft.cse1110.andy.execution.metatest.evaluators.MetaEvaluator;
-import nl.tudelft.cse1110.andy.execution.metatest.evaluators.StringReplacementEvaluator;
-import nl.tudelft.cse1110.andy.execution.metatest.implementations.LibraryMetaTest;
 
 public abstract class AbstractMetaTest implements MetaTest {
 
@@ -25,33 +20,6 @@ public abstract class AbstractMetaTest implements MetaTest {
     @Override
     public int getWeight() {
         return weight;
-    }
-
-    public static AbstractMetaTest withStringReplacement(int weight, String name, String old, String replacement) {
-        MetaEvaluator evaluator = new StringReplacementEvaluator(old, replacement);
-        return new LibraryMetaTest(weight, name, evaluator);
-    }
-
-    public static AbstractMetaTest withStringReplacement(String name, String old, String replacement) {
-        return withStringReplacement(1, name, old, replacement);
-    }
-
-    public static AbstractMetaTest withLineReplacement(int weight, String name, int start, int end, String replacement) {
-        MetaEvaluator evaluator = new LineReplacementEvaluator(start, end, replacement);
-        return new LibraryMetaTest(weight, name, evaluator);
-    }
-
-    public static AbstractMetaTest withLineReplacement(String name, int start, int end, String replacement) {
-        return withLineReplacement(1, name, start, end, replacement);
-    }
-
-    public static AbstractMetaTest insertAt(int weight, String name, int lineToInsertStartingIn1, String contentToAdd) {
-        MetaEvaluator evaluator = new InsertAtEvaluator(lineToInsertStartingIn1, contentToAdd);
-        return new LibraryMetaTest(weight, name, evaluator);
-    }
-
-    public static AbstractMetaTest insertAt(String name, int lineToInsertStartingIn1, String contentToAdd) {
-        return insertAt(1, name, lineToInsertStartingIn1, contentToAdd);
     }
 
     @Override
