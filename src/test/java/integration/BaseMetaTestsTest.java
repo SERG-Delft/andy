54153cab9737ae9df8049de6403ca0b6575976cc
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-29 23:55:31 +0200

    Reduce code duplication

--- src/test/java/integration/BaseMetaTestsTest.java
+++ src/test/java/integration/BaseMetaTestsTest.java
@@ -6,21 +6,19 @@ import org.assertj.core.api.Condition;
 public abstract class BaseMetaTestsTest extends IntegrationTestBase {
 
     public static Condition<? super MetaTestsResult> passedMetaTest(String metaTestName) {
-        return new Condition<>() {
-            @Override
-            public boolean matches(MetaTestsResult value) {
-                return value.getMetaTestResults().stream()
-                        .anyMatch(m -> m.getName().equals(metaTestName) && m.succeeded());
-            }
-        };
+        return metaTestState(metaTestName, true);
     }
 
     public static Condition<? super MetaTestsResult> failedMetaTest(String metaTestName) {
+        return metaTestState(metaTestName, false);
+    }
+
+    public static Condition<? super MetaTestsResult> metaTestState(String metaTestName, boolean success) {
         return new Condition<>() {
             @Override
             public boolean matches(MetaTestsResult value) {
                 return value.getMetaTestResults().stream()
-                        .anyMatch(m -> m.getName().equals(metaTestName) && !m.succeeded());
+                        .anyMatch(m -> m.getName().equals(metaTestName) && m.succeeded() == success);
             }
         };
     }
