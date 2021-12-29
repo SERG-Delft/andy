fdd931c781ed5501ab7d3c98c15251826fdfe62a
Martin Mladenov <m.mladenov@student.tudelft.nl>
2021-12-30 00:44:43 +0200

    Add external process meta test integration tests

---
+++ src/test/java/integration/ExternalProcessMetaTestsTest.java
@@ -0,0 +1,76 @@
+package integration;
+
+import nl.tudelft.cse1110.andy.execution.mode.Action;
+import nl.tudelft.cse1110.andy.result.Result;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.time.Duration.ofSeconds;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+
+public class ExternalProcessMetaTestsTest extends BaseMetaTestsTest {
+
+    private static final String EXTERNAL_PROCESS_LOCAL_CONNECTION = "/andy_test_external_process_local_connection.sh";
+    private static final String EXTERNAL_PROCESS_LOCAL_CONNECTION_META_TEST_FAIL_1 =
+            "/andy_test_external_process_local_connection_meta_test_fail_1.sh";
+    private static final String EXTERNAL_PROCESS_LOCAL_CONNECTION_META_TEST_FAIL_2 =
+            "/andy_test_external_process_local_connection_meta_test_fail_2.sh";
+
+    @BeforeAll
+    static void copyShellScripts() throws IOException {
+        String tmp = getTempDirectory();
+
+        Files.writeString(Path.of(tmp + EXTERNAL_PROCESS_LOCAL_CONNECTION), """
+                echo "initSignal"
+                while true; do echo "HTTP/1.1 200 OK\\nContent-Length: 5\\n\\nhello" | nc -l 8086; done
+                """);
+
+        Files.writeString(Path.of(tmp + EXTERNAL_PROCESS_LOCAL_CONNECTION_META_TEST_FAIL_1), """
+                echo "initSignal"
+                while true; do echo "HTTP/1.1 200 OK\\nContent-Length: 3\\n\\nbye" | nc -l 8086; done
+                """);
+
+        Files.writeString(Path.of(tmp + EXTERNAL_PROCESS_LOCAL_CONNECTION_META_TEST_FAIL_2), """
+                echo "initSignal"
+                while true; do echo "HTTP/1.1 200 OK\\nContent-Length: 15\\n\\nauf wiedersehen" | nc -l 8086; done
+                """);
+    }
+
+    private static String getTempDirectory() {
+        return System.getProperty("java.io.tmpdir");
+    }
+
+    @AfterAll
+    static void shellCleanup() throws IOException {
+        String tmp = getTempDirectory();
+        Files.deleteIfExists(Path.of(tmp + EXTERNAL_PROCESS_LOCAL_CONNECTION));
+        Files.deleteIfExists(Path.of(tmp + EXTERNAL_PROCESS_LOCAL_CONNECTION_META_TEST_FAIL_1));
+        Files.deleteIfExists(Path.of(tmp + EXTERNAL_PROCESS_LOCAL_CONNECTION_META_TEST_FAIL_2));
+    }
+
+    @Test
+    void localConnectionWithMetaTestsTest() {
+        assertTimeoutPreemptively(ofSeconds(30), () -> {
+            Result result = run(Action.FULL_WITH_HINTS, "EmptyLibrary", "ExternalProcessLocalConnectionSolution",
+                    "ExternalProcessLocalConnectionWithMetaTestsConfiguration");
+
+            assertThat(result.hasFailed()).isFalse();
+            assertThat(result.hasGenericFailure()).isFalse();
+
+            assertThat(result.getMetaTests().getPassedMetaTests()).isEqualTo(2);
+            assertThat(result.getMetaTests().getTotalTests()).isEqualTo(5);
+
+            assertThat(result.getMetaTests())
+                    .has(failedMetaTest("example of a failing meta test"))
+                    .has(failedMetaTest("example of another failing meta test"))
+                    .has(passedMetaTest("example of a passing meta test"));
+        });
+    }
+}
