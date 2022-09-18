5afcf809c5111a4c80eded453586744b5e4c2018
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-09-18 14:22:18 +0200

    Add required code check integration tests

--- src/test/java/integration/CodeChecksTest.java
+++ src/test/java/integration/CodeChecksTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.not;
 
 public class CodeChecksTest extends IntegrationTestBase {
 
@@ -31,6 +32,33 @@ public class CodeChecksTest extends IntegrationTestBase {
     }
 
     @Test
+    void requiredCodeChecksPass() {
+        Result result = run( "SoftWhereLibrary", "SoftWhereTests", "SoftWhereConfigWithRequiredCodeChecksPassingConfiguration");
+
+        assertThat(result)
+                .has(checksScore(2,5))
+                .has(requiredCodeCheck("Trip Repository should be mocked required", true, 1))
+                .has(requiredCodeCheck("getTripById should be set up required", true, 1))
+                .has(codeCheck("Trip Repository should be mocked", true, 1))
+                .has(codeCheck("Trip should be mocked", false, 3)) // this check makes no sense, just for the check to fail
+                .has(codeCheck("getTripById should be set up", true, 1));
+    }
+
+    @Test
+    void requiredCodeChecksFail() {
+        Result result = run( "SoftWhereLibrary", "SoftWhereTests", "SoftWhereConfigWithRequiredCodeChecksFailingConfiguration");
+
+        assertThat(result)
+                .has(checksScore(0,0))
+                .has(requiredCodeCheck("Trip Repository should be mocked required", true, 1))
+                .has(requiredCodeCheck("getTripById should be set up required", true, 1))
+                .has(requiredCodeCheck("Trip should be mocked required", false, 1))
+                .has(not(codeCheck("Trip Repository should be mocked", true, 1)))
+                .has(not(codeCheck("Trip should be mocked", false, 3)))
+                .has(not(codeCheck("getTripById should be set up", true, 1)));
+    }
+
+    @Test
     void noChecks() {
         Result result = run( "SoftWhereLibrary", "SoftWhereTests", "SoftWhereConfiguration");
         assertThat(result.getCodeChecks().hasChecks()).isFalse();
@@ -53,6 +81,19 @@ public class CodeChecksTest extends IntegrationTestBase {
             @Override
             public boolean matches(Result value) {
                 return value.getCodeChecks().getCheckResults().stream().anyMatch(cc ->
+                        cc.getWeight() == weight &&
+                                cc.passed() == pass &&
+                                cc.getDescription().equals(name));
+
+            }
+        };
+    }
+
+    public static Condition<Result> requiredCodeCheck(String name, boolean pass, int weight) {
+        return new Condition<>() {
+            @Override
+            public boolean matches(Result value) {
+                return value.getRequiredCodeChecks().getCheckResults().stream().anyMatch(cc ->
                         cc.getWeight() == weight &&
                                 cc.passed() == pass &&
                                 cc.getDescription().equals(name));
