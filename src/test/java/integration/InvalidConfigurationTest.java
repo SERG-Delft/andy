8da500d7dd8875032fe0976bf469dfb5bfca9674
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-10-07 12:39:51 +0200

    Add integration test for invalid configuration

---
+++ src/test/java/integration/InvalidConfigurationTest.java
@@ -0,0 +1,33 @@
+package integration;
+
+import nl.tudelft.cse1110.andy.writer.ResultWriter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class InvalidConfigurationTest extends IntegrationTestBase {
+
+    private ResultWriter writer;
+
+    @BeforeEach
+    public void setup() {
+        writer = mock(ResultWriter.class);
+    }
+
+    @Override
+    protected ResultWriter getWriter() {
+        return writer;
+    }
+
+    @Test
+    void nonzeroWeightButZeroTotal() {
+        assertThrows(RuntimeException.class, () -> {
+            run("SoftWhereLibrary", "SoftWhereTests", "SoftWhereConfiguration");
+        });
+        verify(writer).uncaughtError(any(), any(RuntimeException.class));
+    }
+}
