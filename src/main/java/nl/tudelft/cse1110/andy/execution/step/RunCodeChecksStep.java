77d37aa23928f9045e500baf8469d319f4fab067
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-10-10 21:26:17 +0200

    Revert "Refactor RunCodeChecksStep"
    This reverts commit a4a19b0f866d01c2806db0d4d50b819148012575.

--- src/main/java/nl/tudelft/cse1110/andy/execution/step/RunCodeChecksStep.java
+++ src/main/java/nl/tudelft/cse1110/andy/execution/step/RunCodeChecksStep.java
@@ -1,9 +1,9 @@
 package nl.tudelft.cse1110.andy.execution.step;
 
 import nl.tudelft.cse1110.andy.codechecker.engine.CheckScript;
+import nl.tudelft.cse1110.andy.execution.Context;
 import nl.tudelft.cse1110.andy.config.DirectoryConfiguration;
 import nl.tudelft.cse1110.andy.config.RunConfiguration;
-import nl.tudelft.cse1110.andy.execution.Context;
 import nl.tudelft.cse1110.andy.execution.ExecutionStep;
 import nl.tudelft.cse1110.andy.result.ResultBuilder;
 
@@ -13,13 +13,17 @@ import static nl.tudelft.cse1110.andy.utils.FilesUtils.findSolution;
 
 public class RunCodeChecksStep implements ExecutionStep {
     @Override
-    public void execute(Context ctx, ResultBuilder result) throws FileNotFoundException {
+    public void execute(Context ctx, ResultBuilder result) {
         DirectoryConfiguration dirCfg = ctx.getDirectoryConfiguration();
         RunConfiguration runCfg = ctx.getRunConfiguration();
 
         CheckScript script = runCfg.checkScript();
-        script.runChecks(findSolution(dirCfg.getWorkingDir()));
-        result.logCodeChecks(script);
+        try {
+            script.runChecks(findSolution(dirCfg.getWorkingDir()));
+            result.logCodeChecks(script);
+        } catch (Exception e) {
+            result.genericFailure(this, e);
+        }
     }
 
     @Override
