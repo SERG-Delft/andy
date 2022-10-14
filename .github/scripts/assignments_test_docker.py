2901a185a170eba3fedc9a6bd256e646aeade4f9
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-10-14 17:59:24 +0200

    Implement logic to run assignment with Docker

--- .github/scripts/assignments_test_docker.py
+++ .github/scripts/assignments_test_docker.py
@@ -1,5 +1,4 @@
 import os, re, sys
-from shutil import copyfile
 
 def get_directories(basedir):
     return [os.path.join(basedir, dir) for dir in os.listdir(basedir) \
@@ -7,20 +6,36 @@ def get_directories(basedir):
                                        and not dir.startswith('.')]
 
 home_dir = '/home/runner/work/andy/andy/assignments'
+output_dir = '/home/runner/work/andy/andy/weblab-docker-v2/testresults'
+test_dir = '/home/runner/work/andy/andy/weblab-docker-v2/tests/github-ci'
 
 commit_hash = os.environ['COMMIT_HASH']
 print(f'Running Andy on {commit_hash}')
 
 expected_andy_version = f'-{commit_hash[:7]} '
 
+# Switch to Docker directory
+os.chdir('./weblab-docker-v2')
+
+# Create test structure
+os.makedirs(test_dir)
+
 pipeline_failed = False
 for category_dir in get_directories(home_dir):
     for assignment_dir in get_directories(category_dir):
-        # Copy the solution to the correct folder.
+        # Remove the contents of the output directory.
+        os.system(f'rm -r {output_dir}')
+
+        # Remove the contents of the test directory.
+        os.system(f'rm -r {test_dir}/*')
+
+        # Copy the assignment to the test folder.
+        os.system(f'cp {assignment_dir}/config/Configuration.java {test_dir}/test.txt')
+        os.system(f'cp {assignment_dir}/solution/*.java {test_dir}/solution.txt')
+        os.system(f'cp {assignment_dir}/src/main/java/delft/*.java {test_dir}/library.txt')
 
         # Run `andy` on the current assignment.
-        
-        output = ''
+        output = os.popen('make github-ci.test').read()
 
         re_score = re.search('Final grade: [0-9]+', output)
         score = int(re_score.group().split()[2]) if re_score else -1
