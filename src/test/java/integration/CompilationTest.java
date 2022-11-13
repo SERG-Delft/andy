0cd7d1af7d7f7a5153ada2c5e341a40872d03bee
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-11-13 13:04:28 +0100

    Add code snippet integration test

--- src/test/java/integration/CompilationTest.java
+++ src/test/java/integration/CompilationTest.java
@@ -2,12 +2,46 @@ package integration;
 
 import nl.tudelft.cse1110.andy.execution.mode.Action;
 import nl.tudelft.cse1110.andy.result.Result;
+import nl.tudelft.cse1110.andy.writer.ResultWriter;
+import nl.tudelft.cse1110.andy.writer.standard.CodeSnippetGenerator;
+import nl.tudelft.cse1110.andy.writer.standard.RandomAsciiArtGenerator;
+import nl.tudelft.cse1110.andy.writer.standard.StandardResultWriter;
+import nl.tudelft.cse1110.andy.writer.standard.VersionInformation;
 import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static nl.tudelft.cse1110.andy.utils.FilesUtils.concatenateDirectories;
+import static nl.tudelft.cse1110.andy.utils.FilesUtils.readFile;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static unit.writer.standard.StandardResultTestAssertions.compilationErrorOnLine;
 
 public class CompilationTest extends IntegrationTestBase {
+    protected VersionInformation versionInformation = new VersionInformation("testVersion", "testBuildTimestamp", "testCommitId");
+    protected RandomAsciiArtGenerator asciiArtGenerator = mock(RandomAsciiArtGenerator.class);
+    protected CodeSnippetGenerator codeSnippetGenerator = new CodeSnippetGenerator();
+
+    protected ResultWriter writer;
+
+    protected ResultWriter buildWriter() {
+        return new StandardResultWriter(versionInformation, asciiArtGenerator, codeSnippetGenerator);
+    }
+
+    @BeforeEach
+    void createWriter() {
+        this.writer = buildWriter();
+    }
+
+    protected String generatedResult() {
+        return readFile(new File(concatenateDirectories(reportDir.toString(), "stdout.txt")));
+    }
+
+    protected void writeResult(Result result) {
+        writer.write(ctx, result);
+    }
 
     @Test
     void compilationOk() {
@@ -33,7 +67,9 @@ public class CompilationTest extends IntegrationTestBase {
 
     @Test
     void compilationWithManyFailures() {
-        Result result = run(Action.TESTS,"MathArraysLibrary","MathArraysDifferentCompilationErrors");
+        Result result = run(Action.TESTS, "MathArraysLibrary", "MathArraysDifferentCompilationErrors");
+
+        writeResult(result);
 
         assertThat(result.getCompilation().successful()).isFalse();
         assertThat(result.getFinalGrade()).isEqualTo(0);
@@ -45,11 +81,25 @@ public class CompilationTest extends IntegrationTestBase {
                 .has(compilationErrorInLine(25))
                 .has(compilationErrorInLine(33))
                 .has(sameCompilationErrorManyTimes("cannot find symbol", 3));
+
+        // Verify that the code snippet is correct and is printed only for the first error
+        String output = generatedResult();
+        assertThat(output)
+                .has(compilationErrorOnLine(21))
+                .has(compilationErrorOnLine(25))
+                .has(compilationErrorOnLine(33))
+                .containsOnlyOnce("""
+                                @MethodSource("generator")
+                                void unique(String description, double[] array, double[] expectedArray) {
+                        -->         assertThat(MathArrays.unique(array)).isEqual(expectedArray);
+                                }
+                        """)
+                .doesNotContain("tc2 = Arguments.of(");
     }
 
     @Test
     void compilationErrorIsInTheConfigFile() {
-        Result result = run(Action.TESTS,"ListUtilsLibrary", "ListUtilsCompilationSuccess", "ListUtilsConfigWithCompilationError");
+        Result result = run(Action.TESTS, "ListUtilsLibrary", "ListUtilsCompilationSuccess", "ListUtilsConfigWithCompilationError");
 
         assertThat(result.getCompilation().successful()).isFalse();
         assertThat(result.getCompilation().hasConfigurationError()).isTrue();
@@ -83,8 +133,6 @@ public class CompilationTest extends IntegrationTestBase {
             }
         };
     }
-
-
 
 
 }
