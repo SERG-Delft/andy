ca52b854c986db47332eaad12407dbe63ec92409
Martin Mladenov <m.mladenov@student.tudelft.nl>
2022-12-24 11:36:58 +0200

    Add boundary test for editor feedback

--- src/test/java/unit/writer/weblab/WebLabResultWriterTest.java
+++ src/test/java/unit/writer/weblab/WebLabResultWriterTest.java
@@ -18,8 +18,8 @@ import static nl.tudelft.cse1110.andy.utils.FilesUtils.concatenateDirectories;
 import static nl.tudelft.cse1110.andy.utils.FilesUtils.readFile;
 import static org.assertj.core.api.Assertions.allOf;
 import static org.assertj.core.api.Assertions.assertThat;
-import static unit.writer.weblab.WebLabEditorFeedbackJsonTestAssertions.*;
 import static unit.writer.standard.StandardResultTestAssertions.finalGradeInXml;
+import static unit.writer.weblab.WebLabEditorFeedbackJsonTestAssertions.*;
 
 public class WebLabResultWriterTest extends StandardResultWriterTest {
 
@@ -52,11 +52,11 @@ public class WebLabResultWriterTest extends StandardResultWriterTest {
     void testLineCoverageInEditorFeedback() {
         Result result = new ResultTestDataBuilder()
                 .withCoverageResult(CoverageResult.build(
-                        4, 7, 5, 8, 1, 2,
+                        5, 7, 5, 8, 1, 2,
                         new CoverageLineByLine(
-                                List.of(1, 2, 3, 7),
+                                List.of(1, 2, 3, 6, 7),
                                 List.of(4),
-                                List.of(5, 6)
+                                List.of(5)
                         )
                 ))
                 .build();
@@ -67,9 +67,38 @@ public class WebLabResultWriterTest extends StandardResultWriterTest {
 
         assertThat(editorFeedbackJson)
                 .has(editorFeedbackFullyCovered(1, 3))
-                .has(editorFeedbackFullyCovered(7, 7))
+                .has(editorFeedbackFullyCovered(6, 7))
                 .has(editorFeedbackPartiallyCovered(4, 4))
-                .has(editorFeedbackNotCovered(5, 6));
+                .has(editorFeedbackNotCovered(5, 5));
+    }
+
+    @Test
+    void testEditorFeedbackCoverageBoundaries() {
+        Result result = new ResultTestDataBuilder()
+                .withCoverageResult(CoverageResult.build(
+                        8, 13, 0, 0, 0, 0,
+                        new CoverageLineByLine(
+                                List.of(4, 7, 13, 9, 3, 1, 10, 5),
+                                List.of(),
+                                List.of(2, 6, 8, 11, 12)
+                        )
+                ))
+                .build();
+
+        writer.write(ctx, result);
+
+        String editorFeedbackJson = editorFeedbackJson();
+
+        assertThat(editorFeedbackJson)
+                .has(editorFeedbackFullyCovered(1, 1))
+                .has(editorFeedbackFullyCovered(3, 5))
+                .has(editorFeedbackFullyCovered(7, 7))
+                .has(editorFeedbackFullyCovered(9, 10))
+                .has(editorFeedbackFullyCovered(13, 13))
+                .has(editorFeedbackNotCovered(2, 2))
+                .has(editorFeedbackNotCovered(6, 6))
+                .has(editorFeedbackNotCovered(8, 8))
+                .has(editorFeedbackNotCovered(11, 12));
     }
 
     @Override
@@ -90,6 +119,4 @@ public class WebLabResultWriterTest extends StandardResultWriterTest {
 
         return xmlIsCorrect;
     }
-
-
 }
