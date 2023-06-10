7bab2a12ca30188c7c798addaf1d77ca5f32ff9c
Martin Mladenov <m.mladenov@student.tudelft.nl>
2023-06-10 14:25:43 +0200

    Fix class name

--- assignments/property-based-testing/unique/config/Configuration.java
+++ assignments/property-based-testing/unique/config/Configuration.java
@@ -21,7 +21,7 @@ public class Configuration extends RunConfiguration {
 
     @Override
     public List<String> classesUnderTest() {
-        return List.of("delft.MathArrays");
+        return List.of("delft.MathArraysPBT");
     }
 
     @Override
