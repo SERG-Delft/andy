09a27427e4e127c440de1e30167d4ad76e70ca36
Rūta Giedrytė <ruta.giedryte@yahoo.com>
2023-07-11 16:28:52 +0200

    Refactored the DoubleBraceInitialization

--- andy/src/main/java/nl/tudelft/cse1110/andy/codechecker/checks/WithinAfterEach.java
+++ andy/src/main/java/nl/tudelft/cse1110/andy/codechecker/checks/WithinAfterEach.java
@@ -1,13 +1,15 @@
 package nl.tudelft.cse1110.andy.codechecker.checks;
 
+import com.google.common.collect.ImmutableSet;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public abstract class WithinAfterEach extends WithinAnnotatedMethod {
 
-    public static Set<String> AFTEREACH_ANNOTATION = new HashSet<>() {{
-        add("AfterEach");
-    }};
+    public static Set<String> AFTEREACH_ANNOTATION = new HashSet<>(ImmutableSet.of(
+        "AfterEach"
+    ));
 
     protected Set<String> annotations() {
         return AFTEREACH_ANNOTATION;
