21c32505e7f45f27255592a40a3b32254066b479
Francesco Hamar <f.hamar@student.tudelft.nl>
2023-07-31 23:16:16 +0200

    Added penalty meta test exectution step

---
+++ andy/src/main/java/nl/tudelft/cse1110/andy/execution/step/RunPenaltyMetaTestsStep.java
@@ -0,0 +1,63 @@
+package nl.tudelft.cse1110.andy.execution.step;
+
+import dev.failsafe.Execution;
+import nl.tudelft.cse1110.andy.config.DirectoryConfiguration;
+import nl.tudelft.cse1110.andy.config.MetaTest;
+import nl.tudelft.cse1110.andy.config.RunConfiguration;
+import nl.tudelft.cse1110.andy.execution.Context.Context;
+import nl.tudelft.cse1110.andy.execution.ExecutionStep;
+import nl.tudelft.cse1110.andy.result.MetaTestResult;
+import nl.tudelft.cse1110.andy.result.ResultBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunPenaltyMetaTestsStep implements ExecutionStep {
+
+    @Override
+    public void execute(Context ctx, ResultBuilder result) {
+        DirectoryConfiguration dirCfg = ctx.getDirectoryConfiguration();
+        RunConfiguration runCfg = ctx.getRunConfiguration();
+
+        int score = 0;
+        int totalWeight = 0;
+
+        ClassLoader currentClassLoader = Thread.currentThread().getContextClassLoader();
+
+        try {
+
+            List<MetaTest> metaTests = runCfg.metaTests();
+            List<MetaTestResult> metaTestResults = new ArrayList<>();
+
+            for (MetaTest metaTest : metaTests) {
+
+                boolean passesTheMetaTest = metaTest.execute(ctx, dirCfg, runCfg);
+
+                if (passesTheMetaTest) {
+                    score += metaTest.getWeight();
+                }
+
+                metaTestResults.add(new MetaTestResult(metaTest.getName(), metaTest.getWeight(), passesTheMetaTest));
+
+                totalWeight += metaTest.getWeight();
+            }
+
+            result.logMetaTests(score, totalWeight, metaTestResults);
+        } catch (Exception ex) {
+            result.genericFailure(this, ex);
+        } finally {
+            /* restore the class loader to the one before meta tests */
+            Thread.currentThread().setContextClassLoader(currentClassLoader);
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof RunMetaTestsStep;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
